47eaabfc6435bf85fce63bd65e38c3bc2bc0afd8
vucodiing <vucodiing@gmail.com>
2021-06-08 15:04:21 +0700

    add header, footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 import "./index.css";
 
 function App() {
-  return <div>LIST CUSTOMER</div>;
+  return (
+    <div className="app">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./header.css";
+export default function Header() {
+  return <div className="header">APP QUẢN LÝ KHÁCH HÀNG</div>;
+}
